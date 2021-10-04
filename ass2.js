0a7cfa4aef05487e4831ab68e2aaf913e82050ae
honeybharatandani <andanihoney106@gmail.com>
2021-10-05 00:28:53 +0530

    updates

---
+++ ass2.js
@@ -0,0 +1,20 @@
+// 2c. Reversing an array: Arrays have a reverse method which changes the array by inverting
+// the order in which its elements appear.For this exercise, write a function,
+// reverseArray.The reverseArray, takes an array as argument and produces a new array that has the same elements
+//     in the inverse order.Without reverse method.
+
+const array = ["A", "B", "C"];
+
+let revArray = array.reverse(function (rev) {
+  return revArray;
+});
+console.log(revArray);
+
+// 2.d. Write a function which check if items of an array are unique?
+// let checkUniqueness;
+// const arrOne = [1, 4, 6, 2, 1];
+// console.log(checkUniqueness(arrOne));
+// false;
+// const arrTwo = [1, 4, 6, 2, 3];
+// console.log(checkUniqueness(arrTwo));
+// true;
