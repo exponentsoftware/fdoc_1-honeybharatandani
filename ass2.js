7fc0be06787a91c61375b063fff48ee9abd86867
honeybharatandani <andanihoney106@gmail.com>
2021-10-05 17:25:23 +0530

    updates

--- ass2.js
+++ ass2.js
@@ -6,6 +6,7 @@ const triangle = () => {
 };
 triangle();
 
+//i+=2  i=i+2
 // 2c. Reversing an array: Arrays have a reverse method which changes the array by inverting
 // the order in which its elements appear.For this exercise, write a function,
 // reverseArray.The reverseArray, takes an array as argument and produces a new array that has the same elements
@@ -18,6 +19,19 @@ let revArray = array.reverse(function () {
 });
 console.log("Reverse of Array is ", revArray);
 
+// 2.b. Write a function which returns array of seven random numbers in a range of 0-9. All the numbers must be unique
+
+let arr = [];
+const sevenRandomNumbers = () => {
+  while (arr.length < 7) {
+    let no = Math.floor(Math.random() * 9) + 1;
+    if (arr.indexOf(no) == -1) {
+      arr.push(no);
+    }
+  }
+  console.log("Random numbers are ", arr);
+};
+sevenRandomNumbers();
 // 2.d. Write a function which check if items of an array are unique?
 // let checkUniqueness;
 // const arrOne = [1, 4, 6, 2, 1];
@@ -26,11 +40,3 @@ console.log("Reverse of Array is ", revArray);
 // const arrTwo = [1, 4, 6, 2, 3];
 // console.log(checkUniqueness(arrTwo));
 // true;
-
-let arrOne = [1, 4, 6, 2, 3];
-let arrTwo = [1, 4, 6, 2, 1];
-
-const checkUniqueness =
-  arrOne || arrTwo.filter((item, i, ar) => ar.indexOf(item) === i);
-
-console.log(checkUniqueness);
